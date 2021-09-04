3c7b27d72b49321e34ddd4962dc780bd56c3e270
nipuna <nipuna@nipunas-MacBook-Pro.local>
2021-09-04 19:20:18 +0530

    url shortener

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ var shortid = require('shortid');
 var app = express();
 var port = process.env.PORT || 3000;
 
-// var database_uri = "mongodb+srv://nipuna:nipuna@cluster0.0dte1.mongodb.net/Cluster0?retryWrites=true&w=majority";
-mongoose.connect(process.env.DB_URI);
-// mongoose.connect(database_uri);
+var database_uri = "mongodb+srv://nipuna:nipuna@cluster0.0dte1.mongodb.net/Cluster0?retryWrites=true&w=majority";
+// mongoose.connect(process.env.DB_URI);
+mongoose.connect(database_uri);
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
